f4b52c26f278b48eb1c67fa14f804508a9e48127
1 <menathlakvindub@gmail.com>
2023-12-29 11:45:38 +0530

    created header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import "./header-style.css";
 
-function Header () {
-    
+function Header() {
+    return (
+        <header className='header-container'>
+            <div className='header-content'>
+                <h1 className='header-title'>menath.dev</h1>
+                <nav className='header-nav'>
+
+                    <div className='nav-item'>
+                        <button><a href="#">Home</a></button>
+                    </div>
+                    <div className='nav-item'>
+                        <button><a href="#">About</a></button>
+                    </div>
+                    <div className='nav-item'>
+                        <button><a href="#">Projects</a></button>
+                    </div>
+                    <div className='nav-item'>
+                        <button><a href="#">Blogs</a></button>
+                    </div>
+                    
+                </nav>
+            </div>
+        </header>
+
+    );
+
 }
 
 export default Header;
