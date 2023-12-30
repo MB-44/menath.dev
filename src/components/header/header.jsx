fbbcd74f2ec9e1bc4273d7584297df2d04b1ebea
1 <menathlakvindub@gmail.com>
2023-12-30 21:40:54 +0530

    heading to resp header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./header-style.css";
 
 
 function Header() {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
     return (
         <header className='header-container'>
             <div className='header-content'>
                 <nav className='header-nav'>
                     <h1 className='header-title'>menath.dev</h1>
 
-                <div className='navbar-items'>
+                <button className='navbar-toggle' onClick={toggleMenu}>
+                    &#9776;
+                </button>
+
+
+                {/* <div className='navbar-items'> */}
+                <div className={`navbar-items ${showMenu ? 'show-menu': ''}`}>
                     <ul>
                         <li><button><a href="#">Home</a></button></li>
                         <li><button><a href="#">About</a></button></li>
