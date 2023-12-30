0346686d89d160f7c47acb7d2354de8a8b7a30d7
1 <menathlakvindub@gmail.com>
2023-12-30 10:39:55 +0530

    added card, more more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './components/header/header';
+import HomePage from './pages/home/home-page';
 
 function App() {
   return (
     <>
-      <Header/>
+      <HomePage />
       
     </>
 
--- src/pages/Home/home-page.jsx
+++ src/pages/Home/home-page.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import Header from "../../components/header/header";
+import styles from "./home.module.css";
+import ProfilePic from "../../assets/profile-pic.jpg";
 
 function HomePage() {
-    
+    return (
+        <>
+        <Header />
 
-
+        <div className={styles.card}>
+            <div className={styles.content}>
+                <h2>Hi, I'm Menath</h2>
+                <h3>Also known as MB-44</h3>
+                <p>I'm a game developer</p>
+            </div>
+            
+            <img src={ProfilePic} className={styles.Img} alt="" />
+        </div>
+        
+        
+        </>
+    );
 }
 
 export default HomePage;
