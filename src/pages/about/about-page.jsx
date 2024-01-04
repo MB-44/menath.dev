214eed4d8cb716c5999f1cce6d646f7ddb5d39d8
1 <menathlakvindub@gmail.com>
2024-01-04 21:56:24 +0530

    few changes

---
+++ src/pages/about/about-page.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import './about.css'
+
+function AboutPage () {
+
+    return (
+        <div className="container">
+
+            <div className="body">
+                <h3>About me</h3>
+                <p>Greetings! I am Menath, a passionate game developer and full-stack developer with a love for creating immersive digital experiences. 
+                    With a strong foundation in both game development and full-stack technologies, 
+                    I thrive on bringing ideas to life through a combination of creativity and technical expertise.</p>
+                <br />
+            </div>
+        </div>
+
+    );
+
+}
+
+export default AboutPage;
