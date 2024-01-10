5cd3a775f91c5dd70c958a9c5cdbd31471b17c02
1 <menathlakvindub@gmail.com>
2024-01-10 23:26:12 +0530

    added _app_func

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,22 @@
-// import { useState } from "react";
-// import { GetServerSidePropsContext } from 'next';
-// import { NotificationsProvider } from "@mantine/notifications";
-// import { getCookie, setCookie } from 'cookies-next';
-// import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import { useState } from "react";
+import next, { GetServerSidePropsContext } from 'next';
+import { NotificationsProvider } from "@mantine/notifications";
+import { getCookie, setCookie } from 'cookies-next';
+import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import { AppProps } from "next/app";
+import { PageBody } from "../components/body";
+import Head from "next/head";
+import PageHeader from "../components/header";
+
+export default function App(props) {
+    const { Component, pageProps } = props;
+    const [colorScheme, setColorScheme] = useState(props.colorScheme);
+
+    const toggleColorScheme = (value) => {
+        const nextColorScheme = value || (colorScheme === "dark" ? "light" : "dark");
+        setColorScheme(nextColorScheme);
+
+        setCookie('mantine-color-scheme', nextColorScheme, {maxAge: 60 * 60 * 24 * 30});
+
+    }
+}
