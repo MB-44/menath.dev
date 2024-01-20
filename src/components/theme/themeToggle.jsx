d911c0d801501cd0e71b22f2783921371dd9d61a
1 <menathlakvindub@gmail.com>
2024-01-20 22:06:02 +0530

    created | themeToggle | almost

--- src/components/theme/themeToggle.jsx
+++ src/components/theme/themeToggle.jsx
@@ -1,10 +1,33 @@
 import { useState, useEffect } from "react"
+import { useTheme } from "next-themes"
+import IconSun from "../icons/iconSun"
+import IconMoon from "../icons/iconMoon"
 
+const ThemeToggle = () => {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+    
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return null;
+    }
 
+    return(
+        <div className="fixed bottom-8 right-8">
+            <button
+                type="button"
+                className="w-11 h-11 rounded-full shadow-md inline-flex justify-center items-center focus:outline-none cardTheme"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            >
+                {theme == 'dark' ? <IconMoon/> : <IconSun/>}
+            </button>
+        </div>
+    );
+}
 
-
-
-
+export default ThemeToggle;
 
 
 
